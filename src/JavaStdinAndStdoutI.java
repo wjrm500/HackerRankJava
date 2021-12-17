3b1002be9b7eb56679406d01e16aea48ae606769
William May <wjrm500@gmail.com>
2021-12-17 16:29:20 +0800

    Java Stdin and Stdout I

---
+++ src/JavaStdinAndStdoutI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaStdinAndStdoutI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] intInputs = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int myInt = scanner.nextInt();
+            intInputs[i] = myInt;
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(intInputs[i]);
+        }
+    }
+}
