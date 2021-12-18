dc2444db3bb87d969a39104f43129d7a3db2b886
William May <wjrm500@gmail.com>
2021-12-18 15:15:30 +0800

    Java Date and Time

---
+++ src/JavaDateAndTime.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class JavaDateAndTime {
+    public static String findDay(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        String dayName = new SimpleDateFormat("EEEE").format(calendar.getTime());
+        return dayName.toUpperCase();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+        String dayName = findDay(year, month, day);
+        System.out.println(dayName);
+    }
+}
