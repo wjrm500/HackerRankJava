0cdf835071bd723fcf65f65115c77f3a49b12742
William May <wjrm500@gmail.com>
2021-12-18 16:06:10 +0800

    Java String Reverse

---
+++ src/Strings/JavaStringReverse.java
@@ -0,0 +1,18 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String A = scanner.next();
+        boolean palindrome = true;
+        for (int i = 0, j = A.length() - 1; i < j; i++, j--) {
+            if (A.charAt(i) != A.charAt(j)) {
+                palindrome = false;
+                break;
+            }
+        }
+        System.out.printf("%s\n", palindrome ? "Yes" : "No");
+    }
+}
