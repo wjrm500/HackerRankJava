2f481659f6f65fc0b54dbdf3daa9aa165293ad43
William May <wjrm500@gmail.com>
2021-12-18 10:57:53 +0800

    Java Stdin and Stdout II

---
+++ src/JavaStdinAndStdoutII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaStdinAndStdoutII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int intInput = Integer.parseInt(scanner.nextLine());
+        double doubleInput = Double.parseDouble(scanner.nextLine());
+        String stringInput = scanner.nextLine();
+        System.out.println("String: " + stringInput);
+        System.out.println("Double: " + doubleInput);
+        System.out.println("Int: " + intInput);
+    }
+}
