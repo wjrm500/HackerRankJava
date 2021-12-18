4ccab44ec48547c95902c93097d5ae8b9c23785e
William May <wjrm500@gmail.com>
2021-12-18 10:38:51 +0800

    Java If-Else

---
+++ src/JavaIfElse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int n = scanner.nextInt();
+        scanner.close();
+        if (n % 2 != 0 || (n >= 6 && n <= 20)) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+    }
+}
