71c0b76807950c944b1ea763fab33f81c1e9231e
William May <wjrm500@gmail.com>
2021-12-18 11:11:29 +0800

    Java Loops II

---
+++ src/JavaLoopsII.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JavaLoopsII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += Math.pow(2, j) * b;
+                System.out.printf("%d ", a);
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
