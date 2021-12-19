a56f849bf60573e1ff493b96ca4182f89c877a82
William May <wjrm500@gmail.com>
2021-12-19 17:03:36 +0800

    Java Comparator fixed

--- src/DataStructures/JavaComparator.java
+++ src/DataStructures/JavaComparator.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class JavaComparator {
     public static class Checker implements Comparator<Player> {
         public int compare(Player a, Player b) {
-            return Integer.compare(b.score, a.score);
+            int firstCmp = Integer.compare(b.score, a.score);
+            return firstCmp == 0 ? a.name.compareTo(b.name) : firstCmp;
         }
     }
 
@@ -24,13 +25,19 @@ public class JavaComparator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        scanner.nextLine(); // Consume newline character
         Player[] players = new Player[n];
         for (int i = 0; i < n; i++) {
             String name = scanner.next();
             int score = scanner.nextInt();
             scanner.nextLine(); // Consume newline character
+//            String line = scanner.nextLine();
+//            String[] tokens = line.split(" ");
+//            String name = tokens[0];
+//            int score = Integer.parseInt(tokens[1]);
             players[i] = new Player(name, score);
         }
+        scanner.close();
         Checker checker = new Checker();
         Arrays.sort(players, checker);
         for (Player player : players) {
